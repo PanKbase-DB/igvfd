ad3165349095eb48beb0496df3953b8e54650ab7
Parul-Kudtarkar <parulbioinf@gmail.com>
2024-10-21 07:59:06 +0000

    primary islet to mappings

--- cdk/infrastructure/config.py
+++ cdk/infrastructure/config.py
@@ -60,7 +60,7 @@ config: Dict[str, Any] = {
                         'construct_id': 'Postgres',
                         'on': True,
                         'props': {
-                            'snapshot_source_db_identifier': DEV_DATABASE_IDENTIFIER,
+                            'snapshot_arn': 'arn:aws:rds:us-west-2:654654139991:snapshot:prod-snap-oct-20',
                             'allocated_storage': 10,
                             'max_allocated_storage': 20,
                             'instance_type': InstanceType.of(
@@ -127,7 +127,7 @@ config: Dict[str, Any] = {
                         'construct_id': 'Postgres',
                         'on': True,
                         'props': {
-                            'snapshot_source_db_identifier': DEV_DATABASE_IDENTIFIER,
+                            'snapshot_arn': 'arn:aws:rds:us-west-2:654654139991:snapshot:prod-snap-oct-20',
                             'allocated_storage': 10,
                             'max_allocated_storage': 20,
                             'instance_type': InstanceType.of(
